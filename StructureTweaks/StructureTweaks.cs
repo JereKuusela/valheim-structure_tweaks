50ece5af25def5ec4f56e646a184f7237ed3dbaf
Jere Kuusela <jere.kuusela@iki.fi>
2022-07-22 20:16:46 +0300

    Initial commit

---
+++ StructureTweaks/StructureTweaks.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using HarmonyLib;
+using ServerSync;
+
+namespace StructureTweaks;
+[HarmonyPatch]
+[BepInPlugin(GUID, NAME, VERSION)]
+public class StructureTweaks : BaseUnityPlugin {
+  public const float INFITE = 1E19F;
+  const string GUID = "structure_tweaks";
+  const string NAME = "Structure Tweaks";
+  const string VERSION = "1.0";
+  public void Awake() {
+    new VersionCheck(GUID)
+    {
+      CurrentVersion = VERSION,
+      DisplayName = NAME
+    };
+    new Harmony(GUID).PatchAll();
+  }
+  private static int HashHealth = "health".GetStableHashCode();
+  public static bool Check(ZNetView view, float defaultValue) => view.IsOwner() && view.GetZDO().GetFloat(HashHealth, defaultValue) < INFITE;
+
+  [HarmonyPatch(typeof(Piece), nameof(Piece.Awake)), HarmonyPostfix]
+  static void ForceTargetNonPlayerBuilt(Piece __instance) {
+    __instance.m_targetNonPlayerBuilt = false;
+  }
+  [HarmonyPatch(typeof(TreeLog), nameof(TreeLog.RPC_Damage)), HarmonyPrefix]
+  static bool TreeLog_RPC_Damage(MineRock5 __instance) => Check(__instance.m_nview, 0f);
+
+  [HarmonyPatch(typeof(WearNTear), nameof(WearNTear.RPC_Damage)), HarmonyPrefix]
+  static bool WearNTear_RPC_Damage(WearNTear __instance) => Check(__instance.m_nview, __instance.m_health);
+
+  [HarmonyPatch(typeof(TreeBase), nameof(TreeBase.RPC_Damage)), HarmonyPrefix]
+  static bool TreeBase_RPC_Damage(TreeBase __instance) => Check(__instance.m_nview, __instance.m_health);
+
+  [HarmonyPatch(typeof(Destructible), nameof(Destructible.RPC_Damage)), HarmonyPrefix]
+  static bool Destructible_RPC_Damage(Destructible __instance) => Check(__instance.m_nview, __instance.m_health);
+
+  [HarmonyPatch(typeof(Character), nameof(Character.RPC_Damage)), HarmonyPrefix]
+  static bool Character_RPC_Damage(Character __instance) => Check(__instance.m_nview, __instance.GetMaxHealth());
+}
---
+++ publish/README.md
@@ -0,0 +1,25 @@
+# Structure Tweaks
+
+Minor tweaks to the game logic that allow creating invulnerable structures.
+
+Install on all clients and on the server.
+
+Check any modding [guide](https://youtu.be/WfvA5a5tNHo) if you don't know how.
+
+# Instructions
+
+This mod does two things:
+
+- Objects with more than 10 000 000 000 000 000 000 (1E19) health won't show any damage numbers.
+- Pieces without creator won't be directly targeted by the enemies.
+
+On its own, this mod doesn't do anything. You need other mods to create objects with absurdly high amoungs of health or pieces without any creator.
+
+For example [Infinity Hammer](https://valheim.thunderstore.io/package/JereKuusela/Infinity_Hammer/) or [World Edit Commands](https://valheim.thunderstore.io/package/JereKuusela/World_Edit_Commands/).
+
+# Changelog
+
+- v1.0
+	- Initial release.
+
+Thanks for Azumatt for creating the mod icon!
